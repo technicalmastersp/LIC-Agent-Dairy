ae0b66f606318bb26bc7f02f07190e78ad5d6182
Shashank S Pandey <shashank@sequent-tech.com>
2025-09-20 00:07:45 +0530

    fixed image url issue for show on ui

--- src/config/siteConfig.tsx
+++ src/config/siteConfig.tsx
@@ -1,11 +1,16 @@
 const siteConfig = {
   title: "Life Insurance Records",
 
+  // on root location
   logo_basic_size: "/logos/logo_basic_size.png",
-  // logo_medium_size: "/logos/logo_medium_size.png",
-  logo_medium_size: "https://lic-agent-dairy.vercel.app/logos/logo_medium_size.png",
+  logo_medium_size: "/logos/logo_medium_size.png",
   logo_full_size: "/logos/logo_full_size.png",
-
+  
+  // in public folder location
+  // logo_basic_size: "/public/logos/logo_basic_size.png",
+  // logo_medium_size: "/public/logos/logo_medium_size.png",
+  // logo_full_size: "/public/logos/logo_full_size.png",
+  
   description: "This is an awesome React web application.",
   author: "Mr. Shashank S Pandey",
   version: "1.0.0",
